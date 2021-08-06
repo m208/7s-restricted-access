f3dc8e4753a867b7b1b698310705be5fd58dc337
AK <avkomissarov@gmail.com>
2021-08-06 22:42:38 +0300

    loading price changed

--- restricted_content/price_editing.php
+++ restricted_content/price_editing.php
@@ -1,5 +1,5 @@
-<script type="text/javascript" src='js/calcv3/loadPrice.js?v=1'></script>
-<script type="text/javascript" src="js/7s_manager_scr.js?v=1"></script>
+<script type="text/javascript" src='js/pricelist/PriceList.js?v=2'></script>
+<script type="text/javascript" src="js/manager/7s_price_editor.js?v=2"></script>
 <div id="db_price" name="db_price" class="table-black"></div>
 <button id="update" class="button green">Сохранить</button>
 <button id="getJSON" class="button cyan">Копировать в JSON</button>
